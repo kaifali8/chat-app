467451d83d0ac784ef905c3f6c79aa4ae589fde4
kaifali8 <kaifali848@gmail.com>
2025-07-17 23:06:25 +0530

    first commit

---
+++ server/middleware/auth.js
@@ -0,0 +1,21 @@
+import User from "../models/user.js";
+import jwt from "jsonwebtoken";
+
+//Middleware to protect routes
+export const protectRoute = async(req,res,next)=>{
+    try{
+        const token = req.headers.token;
+
+        const decode = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decode.userId).select("-password");
+
+        if(!user){
+            return res.json({sucess:false,message:"User not found"});
+        }
+        req.user = user;
+        next();
+    }catch(error){
+        console.log(error.message);
+        res.json({success:false,message:error.message});
+    }
+}
